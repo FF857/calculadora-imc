32c5aa2e2d7ea6202419189906028e01a293e020
FF857 <eufelipe857@gmail.com>
2023-02-05 02:45:26 -0300

    add conteudo script

--- script.js
+++ script.js
@@ -11,21 +11,23 @@ function calcular(){
     var resultado_final = resultado.toFixed(1);
 
     if(resultado_final < 18.5){
-        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: magreza";
-    }
-    if(resultado_final > 18.5 && resultado_final < 24.9) {
-        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: normal";
-    }
-    if(resultado_final > 25.0 && resultado_final < 29.9) {
-        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: sobrepeso";
-    }
-    if(resultado_final > 30.0 && resultado_final < 39.9) {
-        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: obesidade";
-    }
-    if(resultado_final > 40.0) {
-        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: obesidade grave";
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: Magreza";
     }
 
-    //document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final;
+    if(resultado_final > 18.5 && resultado_final < 24.9) {
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: Normal";
+    }
+
+    if(resultado_final > 25.0 && resultado_final < 29.9) {
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: Sobrepeso";
+    }
+
+    if(resultado_final > 30.0 && resultado_final < 39.9) {
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: Obesidade";
+    }
+
+    if(resultado_final > 40.0) {
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado_final + " Classificação: Obesidade grave";
+    }
 
 }
